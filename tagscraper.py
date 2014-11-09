d06b79584043972b6303adcb6162e614669527a2
Paul Andres Young <paulandresyoung@gmail.com>
2014-11-08 17:55:27 -0800

    Improved tagscraper

--- tagscraper.py
+++ tagscraper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from os import path
 import json
 
@@ -12,33 +13,76 @@ from tokens import key, secret
 auth = tweepy.AppAuthHandler(key, secret)
 api = tweepy.API(auth)
 
+Cursor = tweepy.Cursor
 
-def link_scraper(query, outdir="pages"):
-    outdir = path.join(path.curdir, outdir)
-    results = api.search(query)
-    n = 0
 
-    for r in results:
-        urls = [u['expanded_url'] for u in r.entities['urls']]
-        for u in urls:
-            print u
-            page = requests.get(u)
-            outpath = path.join(outdir, str(n) + '.html')
-            n += 1
-            outfile = open(outpath, 'w')
+def page_download(url, outpath):
+    """Copies the file at url to outpath"""
+    outfile = open(outpath, 'w')
+    page = requests.get(url)
+    text = page.text.encode('utf8', 'ignore')
+    outfile.write(text)
 
-            # handle unrecognized characters
-            text = page.text.encode('utf8', 'ignore')
+    return True
 
-            outdict = {'url': u, 'text': text, 'tweet', r.text}
 
-            outfile.write(json.dumps(outdict))
-            outfile.close()
+def get_tweets(query, n=100):
+    """Searchs for tweets with query and returns info about first n"""
+    out = []
+    for tweet in Cursor(api.search, q=query).items(n):
+        # get details for each tweet
+        info = {
+            "text": tweet.text,
+            "full_urls": [u['expanded_url'] for u in tweet.entities['urls']],
+            "short_urls": [u['url'] for u in tweet.entities['urls']],
+            "retweet_count": tweet.retweet_count,
+            "created_at": tweet.created_at.isoformat(),
+            "possibly_sensitive": tweet.possibly_sensitive,
+            "id": tweet.id
+        }
+
+        out.append(info)
+
+    return out
+
+
+def tweet_scraper(tweet_list, outdir):
+    """Scrapes urls from a list of tweets"""
+    for tweet in tweet_list:
+        for i, url in enumerate(tweet['full_urls']):
+            outpath = path.join(
+                outdir, str(tweet['id']) + "_" + str(i) + ".html"
+            )
+            
+            page_download(url, outpath)
+
+
+def scraper(outdir, query, n=100):
+
+    if not path.exists(outdir):
+        #check if path exists and make otherwise
+        os.makedirs(outdir)
+
+    tweets = get_tweets(query, n)
+
+    # create manifest file of tweets
+    manifest = open(path.join(outdir, "manifest.json"), 'w')
+    manifest.write(json.dumps(tweets))
+    manifest.close()
+
+    #scrape tweets
+    tweet_scraper(tweets, outdir)
+
+
+def scrape_hashtag(hashtag, outdir, n=100):
+    """Scrapes n links from hashtag on twitter and places files in outdir"""
+    query = "#{} filter:links".format(hashtag)
+    scraper(outdir, query, n)
 
 
 def main():
-    query = "#Election2014 filter:links"
-    link_scraper(query)
+    outdir = path.join(path.curdir, "data", "Election2014")
+    scrape_hashtag("Election2014", outdir)
 
 if __name__ == '__main__':
     main()
